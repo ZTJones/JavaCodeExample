90841dc3552d560a18c19d5f7d2061f589771817
John Deutscher <johndeu@microsoft.com>
2022-01-07 11:29:46 -0800

    modified

--- example1/demo/src/main/java/com/example/App.java
+++ example1/demo/src/main/java/com/example/App.java
@@ -1,11 +1,15 @@
 package com.example;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Random;
+import java.util.UUID;
+ 
 // As a heads up, many of these were explored but not used.
 import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.http.rest.Response;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
+import com.azure.core.util.Context;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.resourcemanager.mediaservices.MediaServicesManager;
@@ -44,29 +48,30 @@ public class App
         
         MediaServicesManager manager = MediaServicesManager.authenticate(credential, profile);
   
-        // Config info, and random variables.  Please make sure you change these to 
-        String resourceGroupName = "johndeu_managedidentity";
-        String accountName = "johndeumanagedidentity";
-        String assetName = "JavaTester1";
+        // Config info, and random variables.  Update the .env file (copy the sample.env to .env file first)
+        String resourceGroupName = dotenv.get("RESOURCEGROUP");
+        String accountName =  dotenv.get("ACCOUNTNAME");
 
+        // SAMPLE CONFIGURATION SETTINGS
+        // ----------------------------------------------------
+        String uniqueString = UUID.randomUUID().toString().split("-")[0];
+        String assetName = "JavaTester1" + uniqueString;
         String fileName = "SampleVideo_1280x720_10mb.mp4"; // This is actually supposed to be a path, but I just placed file in project root
         String blobName = "movie.mp4"; // This is the name of the blob we transfer upload our video into
-
         String transformName = "CustomTransformJava";
-
-        String jobName = "TestJavaJob";
-
-        String outputName = "TestJavaOutput";
-
+        String jobName = "TestJavaJob" + uniqueString;
+        String outputName = "TestJavaOutput" + uniqueString;
+        
         // I just sort of enabled everything on the SAS token in order to get it to work. 
-        String SAS = "https://johndeumanagedstorage.blob.core.windows.net/?sv=2020-08-04&ss=b&srt=sco&sp=rwdlacitx&se=2022-01-08T09:12:45Z&st=2022-01-07T01:12:45Z&spr=https&sig=yJe0FvHIZc%2Fg1eAplzuMNPDg%2Bs3tYOXNhBJYTp9TyW8%3D";
-        String storageURL = "https://johndeumanagedstorage.blob.core.windows.net";
+        String sasUrl = dotenv.get ("REMOTESTORAGEACCOUNTSAS");
+        String storageURL = "https://" + dotenv.get("STORAGEACCOUNTNAME") + ".blob.core.windows.net";
+        //-----------------------------------------------------
 
         // Getting list of streaming endpoints, then printing them as an example of Iterables
         PagedIterable<StreamingEndpoint> endpoints = manager.streamingEndpoints().list(resourceGroupName, accountName);
         
         for(StreamingEndpoint endpoint : endpoints){
-            System.out.println("Testing loop " + endpoint.name() );
+            System.out.printf("/nStreaming Endpoint found: %s", endpoint.name() );
         }
         
         // Now we're going to work on posting a job, and all that entails
@@ -78,13 +83,13 @@ public class App
         
         // We then create (or update) an asset. It returns an Asset, which is stored in the AssetInner "test." 
         AssetInner test = ams.getAssets().createOrUpdate(resourceGroupName, accountName, assetName, inner); // This doesn't change "inner." 
-        System.out.println(test.container());
+        System.out.printf("/nCreated Asset : %s/n", test.name());
         
         // Next, we need to change gears and head over to storage SDK in order to upload our video.
         // Let's try to authenticate. 
         BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
         .endpoint(storageURL)
-        .sasToken(SAS)
+        .sasToken(sasUrl)
             .containerName(test.container())
             .buildClient();
             
@@ -96,7 +101,7 @@ public class App
         // blobClient.uploadFromFile(fileName);
         
         // Create preset
-        EncoderNamedPreset namedPreset = EncoderNamedPreset.AACGOOD_QUALITY_AUDIO;
+        EncoderNamedPreset namedPreset = EncoderNamedPreset.CONTENT_AWARE_ENCODING;
         BuiltInStandardEncoderPreset preset = new BuiltInStandardEncoderPreset().withPresetName(namedPreset);
         
         // Create Output
@@ -127,7 +132,10 @@ public class App
 
         // and from here we can go ahead and create that job.  Check the portal for when it's done.  Consider using
         // createWithResponse and exploring the methods under the AzureMediaServices object. 
-        ams.getJobs().create(resourceGroupName, accountName, transformName, jobName, jobInner);
+        Response<JobInner> job = ams.getJobs().createWithResponse(resourceGroupName, accountName, transformName, jobName, jobInner,new Context("key", "value") );
+
+        System.out.printf("/nJob submitted: %s", job.getValue().name());
+        
 
     }
 }
